7811913639dbcfd513320b58964b953387727a45
musacanminaz <can@marsalasoft.com>
2021-03-31 16:07:22 +0300

    hash list error updated

--- pages/block/[hash].js
+++ pages/block/[hash].js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 import moment from "moment";
 
-const BlockDetails = ({ data }) => {
+const BlockDetails = ({ data, lastBlockIndex }) => {
   console.log(data);
   const responsive = { xl: 12, lg: 14, md: 24, sm: 24, xs: 24 };
 
@@ -20,69 +20,87 @@ const BlockDetails = ({ data }) => {
     <AppLayout>
       <Row style={{ marginBottom: 20 }}></Row>
       <Row gutter={[10, 20]}>
-        <Card style={carStyle}>
-          <Col span={20}>
-            <h1>Block Details - {data.index}</h1>
-            <p>Hash - {data.hash}</p>
-          </Col>
+        <Col {...responsive}>
+          <Card style={carStyle}>
+            <Col span={20}>
+              <h1>Block Details - {data.index}</h1>
+              <p>Hash - {data.hash}</p>
+            </Col>
 
-          <Col span={20}>
-            <Row>
-              <Col span={6}>
-                <p>Height</p>
-              </Col>
-              <Col span={18}>
-                <p>{data.index}</p>
-              </Col>
-            </Row>
-            <Row>
-              {" "}
-              <Col span={6}>
-                <p>Timestamp</p>
-              </Col>
-              <Col span={18}>
-                {data.timestamp} -{" "}
-                <small>{moment(data.timestamp).toLocaleString()}</small>
-              </Col>
-            </Row>
-            <Row>
-              <Col span={6}>
-                <p>Previous Hash</p>
-              </Col>
-              <Col span={18}>
-                <p>
-                  <a href={`/block/${data.previousHash}`}>
-                    {data.previousHash}
-                  </a>
-                </p>
-              </Col>
-            </Row>
-            <Row>
-              <Col span={6}>
-                <p>Balance</p>
-              </Col>
-              <Col span={18}>
-                <p>{data.figerBalance}</p>
-              </Col>
-            </Row>
-            <Row>
-              <Col span={6}>
-                <p>TxIds</p>
-              </Col>
-              <Col span={18}>
-                <p>
-                  {data.data.map((item, index) => {
-                    return (
-                      <a href={`/tx/${item.id}`} key={index}>
-                        {item.id}
-                      </a>
-                    );
-                  })}
-                </p>
-              </Col>
-            </Row>
-          </Col>
-        </Card>
+            <Col span={24}>
+              <Row>
+                <Col span={6}>
+                  <p>Height</p>
+                </Col>
+                <Col span={18}>
+                  <p>{data.index}</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={6}>
+                  <p>Difficulty</p>
+                </Col>
+                <Col span={18}>
+                  <p>{data.difficulty}</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={6}>
+                  <p>Confirmations</p>
+                </Col>
+                <Col span={18}>
+                  <p>{data.index}</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={6}>
+                  <p>Timestamp</p>
+                </Col>
+                <Col span={18}>
+                  {data.timestamp} -{" "}
+                  <small>{moment(data.timestamp).toLocaleString()}</small>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={6}>
+                  <p>Previous Hash</p>
+                </Col>
+                <Col span={18}>
+                  <p>
+                    <a href={`/block/${data.previousHash}`}>
+                      {data.previousHash}
+                    </a>
+                  </p>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={6}>
+                  <p>Balance</p>
+                </Col>
+                <Col span={18}>
+                  <p>{data.figerBalance}</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={6}>
+                  <p>TxIds</p>
+                </Col>
+                <Col span={18}>
+                  <p>
+                    {data.data.map((item, index) => {
+                      return (
+                        <a href={`/tx/${item.id}`} key={index}>
+                          {item.id}
+                          <br />
+                        </a>
+                      );
+                    })}
+                  </p>
+                </Col>
+              </Row>
+            </Col>
+          </Card>
+        </Col>
       </Row>
     </AppLayout>
   );
